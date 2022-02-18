f245722f63d62168cee52670110c75d462435600
Samet Uslu <samet_u@hotmail.de>
2022-02-18 14:30:43 +0100

    create my @Service

--- backend/src/main/java/de/uslu/TodoService.java
+++ backend/src/main/java/de/uslu/TodoService.java
@@ -2,7 +2,24 @@ package de.uslu;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class TodoService {
+
+    private final TodoRepo todoRepo;
+
+    public TodoService(TodoRepo todoRepo) {
+        this.todoRepo = todoRepo;
+    }
+
+    public Collection<Todo> list() {
+        return todoRepo.list();
+    }
+
+    public void createTodo(Todo todo) {
+        todoRepo.createTodo(todo);
+    }
+
 
 }
