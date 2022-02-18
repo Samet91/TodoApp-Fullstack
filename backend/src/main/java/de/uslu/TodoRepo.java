8ee06e71b7e9beca7a8253b23324c366cbb24a3b
Samet Uslu <samet_u@hotmail.de>
2022-02-18 14:30:10 +0100

    create Hashmap

--- backend/src/main/java/de/uslu/TodoRepo.java
+++ backend/src/main/java/de/uslu/TodoRepo.java
@@ -2,43 +2,21 @@ package de.uslu;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.HashMap;
+
 @Repository
 public class TodoRepo {
 
-    private int id;
-    private String title;
-    private boolean completed;
+    private final HashMap<String,Todo> todos = new HashMap<>();
 
-    public TodoRepo() {
+
+    public Collection<Todo> list() {
+        return todos.values();
     }
 
-    public TodoRepo(int id, String title, boolean completed) {
-        this.id = id;
-        this.title = title;
-        this.completed = completed;
+    public void createTodo(Todo todo) {
+        todos.put(todo.getId(),todo);
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 }
