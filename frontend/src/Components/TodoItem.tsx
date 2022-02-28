33be007e8e8a19a885edf9ed1b5616458c9112bb
Samet Uslu <samet_u@hotmail.de>
2022-02-28 13:21:54 +0100

    create 2 functions via props

--- frontend/src/Components/TodoItem.tsx
+++ frontend/src/Components/TodoItem.tsx
@@ -1,19 +1,43 @@
-import { TodoProps } from "../model";
+import { Todo } from "../model";
 import styled from "styled-components";
 
-export default function TodoItem(props: TodoProps) {
+interface TodoItemProps {
+  todo: Todo;
+  onTodoDeletion: () => void;
+  onTodoChange: (todos: Array<Todo>) => void;
+}
+
+export default function TodoItem(props: TodoItemProps) {
   const deleteTodo = () => {
-    fetch(`http://localhost:8080/todos/${props.id}`, {
+    fetch(`http://localhost:8080/todo/${props.todo.id}`, {
       method: "DELETE",
-    }).then(response => props.onItemChange());
+    }).then(() => props.onTodoDeletion());
+  };
+
+  const toggle = () => {
+    fetch(`http://localhost:8080/todo/${props.todo.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: props.todo.title,
+        description: props.todo.description,
+        completed: props.todo.completed,
+      }),
+    })
+      .then((response) => response.json())
+      .then((todosFromBackend: Array<Todo>) =>
+        props.onTodoChange(todosFromBackend)
+      );
   };
 
   return (
     <Card>
-      <Title>{props.title}</Title>
-      <Description>{props.description}</Description>
-      <Completed>{props.completed}</Completed>
-      <button onClick={() => deleteTodo()}>delete</button>
+      <Title>{props.todo.title}</Title>
+      <Description>{props.todo.description}</Description>
+      <Completed>{props.todo.completed}</Completed>
+      <button onClick={deleteTodo}>delete</button>
     </Card>
   );
 }
@@ -21,16 +45,16 @@ export default function TodoItem(props: TodoProps) {
 const Card = styled.div`
   border: solid 2px;
   margin: 5px;
-`
+`;
 
 const Title = styled.h3`
   margin: 5px;
-`
+`;
 
 const Description = styled.p`
   margin: 5px;
-`
+`;
 
 const Completed = styled.p`
   margin: 5px;
-`
+`;
