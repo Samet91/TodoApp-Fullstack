04a40db16d521e92bbb466380ad1655e37218a64
Samet Uslu <samet_u@hotmail.de>
2022-02-28 13:20:37 +0100

    implement function PUT

---
+++ frontend/src/Components/TodoForm.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Todo } from "../model";
+
+interface TodoFormProps {
+  onTodosChange: (todo: Array<Todo>) => void;
+}
+
+export default function TodoForm(todo: TodoFormProps) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [completed, setCompleted] = useState(false);
+
+  const addTodo = () => {
+    fetch("http://localhost:8080/todo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        completed: completed,
+      }),
+    })
+      .then((response) => response.json())
+      .then((todosFromBackend: Array<Todo>) =>
+        todo.onTodosChange(todosFromBackend)
+      );
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={(e) => setCompleted(!completed)}
+      />
+      <button onClick={addTodo}>Senden</button>
+    </div>
+  );
+}
